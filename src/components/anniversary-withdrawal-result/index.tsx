ca08187f8292d00c1495a85aa61b5ef7705391f2
Jose Machoski <jose.machoski@gmail.com>
2025-09-17 23:23:05 -0300

    feat: implementing loading at result page

--- src/components/anniversary-withdrawal-result/index.tsx
+++ src/components/anniversary-withdrawal-result/index.tsx
@@ -2,15 +2,18 @@
 
 import { useAnniversaryWithdrawal } from '@/contexts/anniversary-withdrawal';
 import { redirect } from 'next/navigation';
+import { useEffect } from 'react';
 
 export function AnniversaryWithdrawalResult() {
-  const { data } = useAnniversaryWithdrawal();
-
-  if (!data.name || !data.amount) {
-    redirect('/');
-  }
+  const { data, isLoading } = useAnniversaryWithdrawal();
 
   const [real, cents] = data?.amount?.toString().split('.');
+
+  useEffect(() => {
+    if ((!data.name || !data.amount) && !isLoading) {
+      redirect('/');
+    }
+  }, [data, isLoading]);
 
   return (
     <div className="bg-white w-full py-16 rounded-xl mx-auto flex flex-col px-8 gap-8 items-center justify-between md:items-start md:flex-row lg:px-20 lg:py-26">
@@ -19,13 +22,23 @@ export function AnniversaryWithdrawalResult() {
           Você pode receber até
         </p>
         <div className="flex items-baseline">
-          <span className="text-md text-gray-600 font-bold mr-1">R$</span>
-          <span className="text-cyan-600 font-semibold text-5xl">
-            {new Intl.NumberFormat('pt-BR', {}).format(Number(real))}
-          </span>
-          <span className="text-cyan-600 font-semibold text-2xl">
-            ,{cents?.toString().padEnd(2, '0') ?? '00'}
-          </span>
+          {isLoading ? (
+            <div className="flex items-baseline space-x-1 w-[200px] animate-pulse h-full">
+              <div className="h-6 bg-gray-300 rounded w-1/5"></div>
+              <div className="h-10 bg-gray-300 rounded w-full"></div>
+              <div className="h-7 bg-gray-300 rounded w-1/5"></div>
+            </div>
+          ) : (
+            <>
+              <span className="text-md text-gray-600 font-bold mr-1">R$</span>
+              <span className="text-cyan-600 font-semibold text-5xl">
+                {new Intl.NumberFormat('pt-BR').format(Number(real))}
+              </span>
+              <span className="text-cyan-600 font-semibold text-2xl">
+                ,{cents?.toString().padEnd(2, '0') ?? '00'}
+              </span>
+            </>
+          )}
         </div>
       </div>
       <p className="text-gray-500 text-center font-bold text-xs md:text-start max-w-[300px] leading-5">
--- src/contexts/anniversary-withdrawal/index.tsx
+++ src/contexts/anniversary-withdrawal/index.tsx
@@ -1,8 +1,10 @@
-import React, { createContext, useContext, useState } from 'react';
+import { usePathname } from 'next/navigation';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 
 export type AnniversaryWithdrawalContextType = {
   data: { name: string; amount: number };
   saveFormData({ name, amount }: { name: string; amount: number }): void;
+  isLoading: boolean;
 };
 
 export const AnniversaryWithdrawalContext = createContext(
@@ -13,6 +15,8 @@ interface AnniversaryWithdrawalProviderProps {
   children: React.ReactNode;
 }
 
+const SESSION_STORAGE_KEY = '@anniversary-withdrawal-data';
+
 export const AnniversaryWithdrawalProvider: React.FC<
   AnniversaryWithdrawalProviderProps
 > = ({ children }) => {
@@ -20,19 +24,49 @@ export const AnniversaryWithdrawalProvider: React.FC<
     name: '',
     amount: 0,
   });
+  const [isLoading, setIsLoading] = useState(true);
+  const pathname = usePathname();
 
   function saveFormData({ name, amount }: { name: string; amount: number }) {
     setData({
       name,
       amount,
     });
+
+    sessionStorage.setItem(
+      SESSION_STORAGE_KEY,
+      JSON.stringify({
+        name,
+        amount,
+      }),
+    );
   }
+
+  useEffect(() => {
+    try {
+      const storedData = sessionStorage.getItem(SESSION_STORAGE_KEY);
+      if (storedData) {
+        setData(JSON.parse(storedData));
+      }
+    } catch (error) {
+      console.error('Failed to parse data from sessionStorage', error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (pathname === '/') {
+      sessionStorage.removeItem(SESSION_STORAGE_KEY);
+    }
+  }, [pathname]);
 
   return (
     <AnniversaryWithdrawalContext.Provider
       value={{
         data,
         saveFormData,
+        isLoading,
       }}
     >
       {children}
